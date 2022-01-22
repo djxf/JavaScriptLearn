e16e731dd4982341e30e48becdd326adc38cd0cc
djxf <1763380121@qq.com>
2022-01-22 15:09:08 +0800

    添加围棋坐标点

--- src/components/go.vue
+++ src/components/go.vue
@@ -1,47 +1,61 @@
-/**
- * 围棋组件
- */
- <template>
-    <canvas id="root"></canvas>
- </template>
+<template>
+    <canvas id="canvas"></canvas>
+</template>
 
 
  <script>
+ const WIDTH = window.innerWidth - 30
+ const HEIGHT = WIDTH
+ const GRID_WIDTH = WIDTH / 18
  export default {
-     methods: {
-         drawGrid(ctx) {
+    methods: {
+        drawGrid(ctx) {
             //<!-- 绘制围棋棋盘 -->
 
             //1 计算棋盘的宽度，高度等于宽度
-            var gridWidth = window.innerWidth;
-            var gridHeight = gridWidth;
+            var gridWidth = WIDTH;
+            var gridHeight = HEIGHT;
 
             //2 计算每个围棋格子的宽度
             var avergeWidth = (gridWidth) / 18;
-            for(var j = 0; j <19; j++) {
+            for(var j = 0; j < 19; j++) {
                 ctx.beginPath();
                 ctx.moveTo(j * avergeWidth, 0);
                 ctx.lineTo(j * avergeWidth, gridHeight);
                 ctx.stroke();
             }
-            for(var i = 0; i <19; i++) {
+            for(var i = 0; i < 19; i++) {
                 ctx.beginPath();
                 ctx.moveTo(0, i * avergeWidth);
                 ctx.lineTo(gridWidth, i * avergeWidth);
                 ctx.stroke();
             }
-         }
-     },
-     mounted:function() {
-         var ctx = document.getElementById("root").getContext("2d");
-         this.drawGrid(ctx)
-     }
+        },
+        drawNinePoint(cxt) {
+            var np = new Array(
+            [GRID_WIDTH * 3,GRID_WIDTH * 3],[GRID_WIDTH * 3,GRID_WIDTH * 9],[GRID_WIDTH * 3,GRID_WIDTH * 15],
+            [GRID_WIDTH * 9,GRID_WIDTH * 3],[GRID_WIDTH * 9,GRID_WIDTH * 9],[GRID_WIDTH * 9,GRID_WIDTH * 15],
+            [GRID_WIDTH * 15,GRID_WIDTH * 3],[GRID_WIDTH * 15,GRID_WIDTH * 9],[GRID_WIDTH * 15,GRID_WIDTH * 15]);
+	
+            for (var i = 0; i < np.length; i++) {
+                //circle
+                cxt.beginPath();
+                cxt.arc(np[i][0],np[i][1],3,0,2*Math.PI,false);
+                cxt.fillStyle="black";
+                cxt.fill();
+            }
+        }
+    },
+    mounted() {
+        let canvas = document.getElementById("canvas")
+        canvas.width = WIDTH;
+        canvas.height = HEIGHT;
+        var ctx = canvas.getContext("2d");
+        this.drawGrid(ctx)
+        this.drawNinePoint(ctx)
+    }
  }
  </script>
 
  <style scoped>
-     #root {
-         width: 100%;
-         height: 100%;
-     }
  </style>
